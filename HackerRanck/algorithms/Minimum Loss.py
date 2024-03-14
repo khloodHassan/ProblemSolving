4b5398b76506e7679fb5af9c508041346a841c62
lenovo <khlood.hassan74@gmail.com>
2024-03-15 00:14:40 +0200

    initial commit

--- HackerRanck/algorithms/Minimum Loss.py
+++ HackerRanck/algorithms/Minimum Loss.py
@@ -1,22 +1,13 @@
 import math
 
-def minimumLoss(price):
-    # Write your code here
-    min_loss = 10000000000000000
-    for p in range(0,len(price)-1):
-        for i in range(p+1,len(price)):
-            if price[p] - price[i] < min_loss and price[p] - price[i] >= 0:
-                min_loss = price[p] - price[i]
-    return min_loss
 
 def minimumLoss(price):
     # Write your code here
-    sortedPrice=sorted([(p,idx) for idx,p in enumerate(price)])
+    sortedPrice=sorted([(p, idx) for idx, p in enumerate(price)])
     minLoss=math.inf
-    n=len(sortedPrice)
-    for i in range(1,n):
-        if sortedPrice[i][1]<sortedPrice[i-1][1]:
-            minLoss=min(minLoss,sortedPrice[i][0]-sortedPrice[i-1][0])
+    for i in range(1, len(sortedPrice)):
+        if sortedPrice[i][1] < sortedPrice[i-1][1]:
+            minLoss = min(minLoss,sortedPrice[i][0]-sortedPrice[i-1][0])
     return minLoss
 if __name__ == '__main__':
 
